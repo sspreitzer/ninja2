9b06000a9d73f9a7ae1cca40688cf806f1abef08
Sascha Spreitzer <sascha@spreitzer.ch>
2015-01-03 13:39:46 +0100

    Add setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+      name = 'j2',
+      version = '0.0.1',
+      description = 'Jinja2 command line client',
+      url = 'https://github.com/sspreitzer/j2',
+      author = 'Sascha Spreitzer',
+      author_email = 'sascha@spreitzer.ch',
+      license = 'MIT',
+      classifiers = [
+                     'Development Status :: 3 - Alpha',
+                     'Programming Language :: Python :: 2',
+                     ],
+      keywords = 'j2 jinja2 python json yaml',
+      install_requires = ['Jinja2'],
+      scripts = ['j2'],
+)
+
