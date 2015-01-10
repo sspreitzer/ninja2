976166788e88088e59693db61c0a7708b53f4d4e
Sascha Spreitzer <sascha@spreitzer.ch>
2015-01-10 11:03:23 +0100

    Version 0.1

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
       name = 'j2',
-      version = '0.0.1',
+      version = '0.1',
       description = 'Jinja2 command line client',
       url = 'https://github.com/sspreitzer/j2',
       author = 'Sascha Spreitzer',
       author_email = 'sascha@spreitzer.ch',
       license = 'MIT',
       classifiers = [
-                     'Development Status :: 3 - Alpha',
+                     'Development Status :: 2 - Beta',
                      'Programming Language :: Python :: 2',
                      ],
       keywords = 'j2 jinja2 python json yaml',
